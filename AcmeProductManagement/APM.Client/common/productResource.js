299ddc966dfe7de36b9dda9b2fa98d38c6a08767
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2018-07-09 15:46:53 +0300

    Retrieving Data

---
+++ AcmeProductManagement/APM.Client/common/productResource.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.service")
+        .factory("productResource",
+            ["$resource",
+            "appSettings",
+            productResource]);
+
+    function productResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/products/:id");
+    }
+}());
